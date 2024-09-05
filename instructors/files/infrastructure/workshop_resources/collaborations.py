8d7dd2ebd655b3f35e84c7ff5cb67a1cfc4f1593
F.C. Martin <f.martin@iknl.nl>
2024-09-05 16:41:45 +0200

    Added GGA and AGOT studies according to review

--- instructors/files/infrastructure/workshop_resources/collaborations.py
+++ instructors/files/infrastructure/workshop_resources/collaborations.py
@@ -17,3 +17,23 @@ studies = [
     "Willow Network: Subset",
     "Redwood Union: Subset",
 ]
+
+agot_studies = [
+    "Oak AGOT2024",
+    "Pine AGOT2024",
+    "Maple AGOT2024",
+    "Cedar AGOT2024",
+    "Birch AGOT2024",
+    "Willow AGOT2024",
+    "Redwood AGOT2024",
+]
+
+gga_studies = [
+    "Oak GGA2024",
+    "Pine GGA2024",
+    "Maple GGA2024",
+    "Cedar GGA2024",
+    "Birch GGA2024",
+    "Willow GGA2024",
+    "Redwood GGA2024",
+]
